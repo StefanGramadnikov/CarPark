9053998a9e9918706f4a4dad645914cb079c8c38
TsenkoTsenov <xprezyourself@gmail.com>
2016-12-06 16:20:29 +0200

    [CP-17] Implemented MyAdsPage and Edit button

---
+++ src/components/views/MyAdsPage.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import MyAd from './AdBox/MyAd';
+import {loadAds} from '../../controllers/CarAdController';
+import $ from 'jquery'
+export default class MyAdsPage extends Component {
+    constructor(props){
+        super(props);
+        this.state = {ads: []};
+        this.onLoadSuccess = this.onLoadSuccess.bind(this);
+    }
+    componentDidMount(){
+        loadAds(this.onLoadSuccess);
+    }
+    onLoadSuccess(response){
+        this.setState({ads: response});
+        //Make all divs same height
+        $( document ).ready(function() {
+            var heights = $(".thumbnail").map(function() {
+                    return $(this).height();
+                }).get(),
+                maxHeight = Math.max.apply(null, heights);
+            $(".thumbnail").height(maxHeight);
+        });
+    }
+    render() {
+        return (
+            <div className="row">
+                {this.state.ads.map((ad, i) =>{if(sessionStorage.getItem('userId') == ad._acl.creator){return <MyAd key={i} title={ad.title} make={ad.make} description={ad.description}
+                                                           model = {ad.model} year = {ad.year}
+                                                           price = {ad.price} adId ={ad._id}/> }})}
+            </div>
+        );
+    }
+}
